79dafcfe08842a746ad8c2acebad81324904ed6b
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-15 01:24:27 -0400

    Add form validation and error messages

--- src/Form.js
+++ src/Form.js
@@ -1,30 +1,34 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-function MainForm() {
+function MainForm({ errors, touched, values }) {
   return (
     <Form>
       <label htmlFor="name">
         Name:
+        {touched.name && errors.name && <p>{errors.name}</p>}
         <Field type="text" name="name" />
       </label>
       <br />
 
       <label htmlFor="email">
         Email:
+        {touched.email && errors.email && <p>{errors.email}</p>}
         <Field type="email" name="email" />
       </label>
       <br />
 
       <label htmlFor="password">
         Password:
+        {touched.password && errors.password && <p>{errors.password}</p>}
         <Field type="password" name="password" />
       </label>
       <br />
 
       <label htmlFor="terms">
         Terms of Service
-        <Field type="checkbox" name="terms" />
+        <Field type="checkbox" name="terms" checked={values.terms} />
       </label>
       <br />
 
@@ -42,6 +46,16 @@ const FormikMainForm = withFormik({
       terms: terms || false
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("This field is required"),
+    email: Yup.string()
+      .email("Email is not valid.")
+      .required("This field is required"),
+    password: Yup.string()
+      .min(8, "Your password must be 8 characters or longer.")
+      .required("This field is required")
+  }),
 
   handleSubmit(values) {
     console.log(values);
